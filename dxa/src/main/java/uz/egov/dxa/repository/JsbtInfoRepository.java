80e3c0d2bf63d015998e7db0a37ac66453cf6f68
sh.almetov <shzd@yandex.ru>
2022-04-22 10:15:07 +0500

    shzd

--- dxa/src/main/java/uz/egov/dxa/repository/JsbtInfoRepository.java
+++ dxa/src/main/java/uz/egov/dxa/repository/JsbtInfoRepository.java
@@ -30,4 +30,23 @@ public interface JsbtInfoRepository extends JpaRepository<JsbtInfo, UUID> {
     @Query("SELECT d FROM JsbtData d WHERE d.jshshir=:jshshir")
     List<JsbtData> findByJSHSHIR(@Param("jshshir") String jshshir);
 
+
+    @Query(value = "select \n" +
+            "max(substr(jshshir, 6, 4))\n" +
+//            ", left(jshshir, 5)||substr(jshshir, 6, 4)||right(jshshir, 5)\n" +
+//            ", left(jshshir, 5)||substr(jshshir, 6, 4)||right(jshshir, 5)\n" +
+//            ", d.jshshir, left(jshshir, 5), right(jshshir, 5), d.*  \n" +
+            "from mh.jstm_data d\n" +
+            "where " +
+            "    left(jshshir, 1)=:index_pola_i_veka_rojdeniya " +
+            "and substr(jshshir, 2, 6)=:data_rojdeniya_ddMMyy " +
+            "and substr(jshshir, 8, 3)=:kod_rayona_goroda " +
+//            "and substr(jshshir, 11, 3)=:poryadkobiy_nomer_grajdanina " +
+//            "and right(jshshir, 1)=:kontrolnaya_ssifra " +
+            "", nativeQuery = true)
+    String genKodGraj(
+            @Param("index_pola_i_veka_rojdeniya") String index_pola_i_veka_rojdeniya,
+            @Param("data_rojdeniya_ddMMyy") String data_rojdeniya_ddMMyy,
+            @Param("kod_rayona_goroda") String kod_rayona_goroda
+    );
 }
--- dxa/src/main/java/uz/egov/dxa/service/JsbtInfoService.java
+++ dxa/src/main/java/uz/egov/dxa/service/JsbtInfoService.java
@@ -13,7 +13,9 @@ import uz.egov.dxa.repository.JsbtInfoRepository;
 
 
 import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Service
@@ -21,9 +23,66 @@ public class JsbtInfoService {
     @Autowired
     JsbtInfoRepository jsbtInfoRepository;
 
-    public List<ResponseJSBT> save(JsbtInfo jsbtInfo){
+    public List<ResponseJSBT> save(JsbtInfo jsbtInfo) {
         List<JsbtData> jsbtDataList = jsbtInfo.getJsbtData();
-        for (JsbtData ls: jsbtDataList) {
+
+        String index_pola_i_veka_rojdeniya = "";
+        String data_rojdeniya_ddMMyy = "";
+        String kod_rayona_gorod = "";
+        String poryadkobiy_nomer_grajdanina = "";
+        String kontrolnaya_ssifra = "";
+        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyy");
+        Calendar calendar = Calendar.getInstance();
+        int century = 0;
+        for (JsbtData ls : jsbtDataList) {
+            //BPol	Боланинг жинси	String	1 та белги, “0” – аёл, “1” - эркак.	[1]
+            index_pola_i_veka_rojdeniya = ls.getBPol();
+            calendar.setTime(ls.getBData());
+            century = (calendar.get(Calendar.YEAR) / 100);
+            if (century == 18 && index_pola_i_veka_rojdeniya.equals("1")) {
+                index_pola_i_veka_rojdeniya = "1";
+            } else if (century == 18 && index_pola_i_veka_rojdeniya.equals("0")) {
+                index_pola_i_veka_rojdeniya = "2";
+            } else if (century == 19 && index_pola_i_veka_rojdeniya.equals("1")) {
+                index_pola_i_veka_rojdeniya = "3";
+            } else if (century == 19 && index_pola_i_veka_rojdeniya.equals("0")) {
+                index_pola_i_veka_rojdeniya = "4";
+            } else if (century == 20 && index_pola_i_veka_rojdeniya.equals("1")) {
+                index_pola_i_veka_rojdeniya = "5";
+            } else if (century == 20 && index_pola_i_veka_rojdeniya.equals("0")) {
+                index_pola_i_veka_rojdeniya = "6";
+            } else
+                index_pola_i_veka_rojdeniya = "?";
+            data_rojdeniya_ddMMyy = sdf.format(ls.getBData());
+            kod_rayona_gorod = ls.getBYTum().substring(0, 3);
+            try {
+                poryadkobiy_nomer_grajdanina = jsbtInfoRepository.genKodGraj(
+                        index_pola_i_veka_rojdeniya,
+                        data_rojdeniya_ddMMyy,
+                        kod_rayona_gorod);
+                century = Integer.getInteger(poryadkobiy_nomer_grajdanina);
+                century = century + 1;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                century = 1;
+            }
+            poryadkobiy_nomer_grajdanina = century + "";
+            kontrolnaya_ssifra = "000";
+            poryadkobiy_nomer_grajdanina =
+                    kod_rayona_gorod.substring(3 - poryadkobiy_nomer_grajdanina.length()) +
+                            poryadkobiy_nomer_grajdanina;
+
+
+
+
+            kontrolnaya_ssifra = "?"; //TODO kontrolnaya_ssifra v JSHSHIR ?
+
+            ls.setJshshir(index_pola_i_veka_rojdeniya +
+                    data_rojdeniya_ddMMyy +
+                    kod_rayona_gorod +
+                    poryadkobiy_nomer_grajdanina +
+                    kontrolnaya_ssifra
+            );
             ls.setInformation(jsbtInfo);
         }
         List<ResponseJSBT> responseList = new ArrayList<ResponseJSBT>();
@@ -31,7 +90,7 @@ public class JsbtInfoService {
         for (JsbtData jsbtData : rt.getJsbtData()) {
             ResponseJSBT response = new ResponseJSBT();
             response.setBJSHSHIR(jsbtData.getJshshir());
-            response.setID_ORG(jsbtData.getId()+"");
+            response.setID_ORG(jsbtData.getId() + "");
             response.setRECEIVE_TIME_ORG(jsbtData.getInstime());
             responseList.add(response);
         }
@@ -41,18 +100,21 @@ public class JsbtInfoService {
     public List<JsbtInfo> readAll() {
         return jsbtInfoRepository.findAll();
     }
+
     public List<JsbtInfo> readAll(Integer pageNo, Integer pageSize) {
         Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by("instime").descending());
         Page<JsbtInfo> pagedResult = jsbtInfoRepository.findAll(paging);
-        if(pagedResult.hasContent()) {
+        if (pagedResult.hasContent()) {
             return pagedResult.getContent();
         } else {
             return new ArrayList<JsbtInfo>();
         }
     }
+
     public List<JsbtInfo> findByDate(Date sana) {
         return jsbtInfoRepository.findByDate(sana);
     }
+
     public List<JsbtData> findByJSHSHIR(String jshshir) {
 //        JsbtData JsbtData = jxInfoRepository.findByJSHSHIR(jshshir).get(0);
 //        JsbtInfo JsbtInfo = JsbtData.getInformation();
